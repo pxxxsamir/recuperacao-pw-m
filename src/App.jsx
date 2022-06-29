9b8fa71ef9669b20074a4c24f4e63d3bccff76be
pxxxsamir <pedrosamir369@gmail.com>
2022-06-29 09:56:38 -0400

    adicionando projeto existente

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Title from './Title';
+import Button from './Button';
+import Noticia from './Noticia';
+import "./App.css"
+
+function App() {
+    const lista_noticias = [
+        {
+            hour: "28/05/2022 09:01",
+            description: "Estão abertas as inscrições para projetos de ensino"
+        },        
+        {
+            hour: "28/05/2022 09:00",
+            description: "Estão abertas as inscrições para projetos de pesquisa"
+        },
+        {
+            hour: "27/05/2022 13:32",
+            description: "Manutenção do Sistema SUAP acontece nesta sexta-feira(27)"
+        },
+    ];
+
+    return (
+        <div className='container'>
+            <Title />
+
+            {lista_noticias.map((noticia) => {
+                return <Noticia horario={noticia.hour} desc={noticia.description} />
+            })}
+
+            <Button frase="Mais Noticias" />
+            <div>Feliz dias das crianças</div>
+
+        </div>
+
+    );
+}
+
+export default App
